c8f612246a6cccb9d2787507d0663f6c7336504c
Mathias Branger <mbranger@sedona.fr>
2022-01-28 09:50:41 +0100

    flyway : setup baseline

--- src/main/resources/db/migration/V1.0.2__add_user.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO USER (id, firstName, lastName, birth, mail)
-VALUES (1, 'Chuck', 'Norris', '2022-01-15', 'chuck.norris@strong.com');
-INSERT INTO USER (id, firstName, lastName, birth, mail)
-VALUES (2, 'Chuck2', 'Norris', '2022-01-15', 'chuck.norris@strong.com');
-INSERT INTO USER (id, firstName, lastName, birth, mail)
-VALUES (3, 'Chuck3', 'Norris', '2022-01-15', 'chuck.norris@strong.com');
-INSERT INTO USER (id, firstName, lastName, birth, mail)
-VALUES (4, 'Chuck3', 'Norris', '2022-01-15', 'chuck.norris@strong.com');
